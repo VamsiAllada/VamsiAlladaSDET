fe2691d2cb67de336175b0d133f3c063175e4228
AlladaV <AlladaV@HBADT4950711.bsg.ad.adp.com>
2021-05-02 15:09:49 +0530

    MultiplicationOfFirst10Numbers

---
+++ SDET Assignments/src/assignments1/MultiplicationOfFirst10Numbers.java
@@ -0,0 +1,17 @@
+package assignments1;
+
+public class MultiplicationOfFirst10Numbers {
+
+	    public static void main(String[] args) {
+
+	       int num = 10, count = 1, total = 1;         
+	       while(count <= num)
+	       {
+	           total = total * count;
+	           count++;
+	       }
+
+	       System.out.println("Sum of first 10 natural numbers is: "+total);
+	    }
+	}
+
