e90d1eebac46893fd81d42f1d698819e43a7f568
AlladaV <AlladaV@HBADT4950711.bsg.ad.adp.com>
2021-05-03 01:20:15 +0530

    RemoveDuplicateStrings

---
+++ SDET Assignments/src/assignments2/RemoveDuplicateStrings.java
@@ -0,0 +1,49 @@
+package assignments2;
+
+public class RemoveDuplicateStrings {
+	package com.test;
+
+	import java.util.ArrayList;
+	import java.util.List;
+
+	public class Program5 {
+		static List<String> words;
+		
+		public static void main(String[] args) {
+			words = new ArrayList<String>();
+			System.out.println("Adding Words..");
+			words.add("THE");
+			words.add("OF");
+			words.add("GUEST");
+			words.add("VERY");
+			words.add("NEXT");
+			words.add("THE");
+			words.add("SORRY");
+			words.add("THANKS");
+			words.add("THE");
+			words.add("REST");
+			words.add("VERY");
+			words.add("GUEST");
+			
+			System.out.println("Printing Words");
+			words.forEach(System.out::println);
+			
+			System.out.println("Removing Duplicate Words");
+			words = removeDuplicateWords(words);
+			words.forEach(System.out::println);
+					
+		}
+		
+		static List<String> removeDuplicateWords(List<String> words){
+			List<String> uniqueWords = new ArrayList<String>();
+			if(!words.isEmpty()) {
+				for(String word : words) {
+					if(!uniqueWords.contains(word)) {
+						uniqueWords.add(word);
+					}
+				}
+			}
+			return uniqueWords;
+		}
+	}
+}
