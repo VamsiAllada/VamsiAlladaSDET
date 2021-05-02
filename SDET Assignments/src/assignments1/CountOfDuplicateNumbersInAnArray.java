265dc28cdb3ce3d18915ce8f4dda932e42afb9c6
AlladaV <AlladaV@HBADT4950711.bsg.ad.adp.com>
2021-05-02 16:24:27 +0530

    CountOfDuplicateNumbersInAnArray

---
+++ SDET Assignments/src/assignments1/CountOfDuplicateNumbersInAnArray.java
@@ -0,0 +1,15 @@
+package assignments1;
+public class CountOfDuplicateNumbersInAnArray { 
+		public static void main(String[] args) {    
+		        int [] arr = new int [] {4, 2, 3, 4, 2, 7, 8, 8, 7};  
+		        int count;
+		        System.out.println("Duplicate elements in given array: ");    
+		        for(int i = 0; i < arr.length; i++) {  
+		            for(int j = i + 1; j < arr.length; j++) {  
+		                if(arr[i] == arr[j])  
+		                    count++;  
+		            }  
+		          System.out.println(count);
+		        }  
+		    }  
+		} 
