0367ec6501891a901258441ab9f5f3aa78855cec
AlladaV <AlladaV@HBADT4950711.bsg.ad.adp.com>
2021-05-02 16:19:14 +0530

    NumberIsEvenOrOddInAnArray

---
+++ SDET Assignments/src/assignments1/NumberIsEvenOrOddInAnArray.java
@@ -0,0 +1,21 @@
+package assignments1;
+
+public class NumberIsEvenOrOddInAnArray {
+		public static void main(String [] args)
+		{  
+		int a[]={1,3,5,6,8,2};  
+		System.out.println("Odd Numbers:");  
+		for(int i=0;i<a.length;i++){  
+		if(a[i]%2!=0){  
+		System.out.println(a[i]);  
+		}  
+		}  
+		System.out.println("Even Numbers:");  
+		for(int i=0;i<a.length;i++){  
+		if(a[i]%2==0){  
+		System.out.println(a[i]);  
+		}  
+		}  
+		} 
+	
+}
