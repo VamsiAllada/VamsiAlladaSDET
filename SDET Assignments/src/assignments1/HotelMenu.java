625b6ebf1447a5d47840419e3774e79933f28824
AlladaV <AlladaV@HBADT4950711.bsg.ad.adp.com>
2021-05-02 16:33:03 +0530

    HotelMenu

---
+++ SDET Assignments/src/assignments1/HotelMenu.java
@@ -0,0 +1,16 @@
+package assignments1;
+Scanner sc=new Scanner(System.in);
+public class HotelMenu {
+	int i,n,count;
+	System.out.println("Enter Item number (1.Dosa, 2.Idly, 3.Pongal): ");
+	i=sc.nextInt();
+	System.out.println("Number of plates: ");
+	n=sc.nextInt();
+	if(i==1)
+		count=20*n;
+	else if(i==2)
+		count=10*n;
+	else
+		count=30*n;
+	System.out.println("Final bill: "+count);
+}
