06cc64b0568a755ab3202eaf2166aad41382dc71
AlladaV <AlladaV@HBADT4950711.bsg.ad.adp.com>
2021-05-03 01:14:57 +0530

    AddEmployees

---
+++ SDET Assignments/src/assignments2/AddEmployees.java
@@ -0,0 +1,50 @@
+package assignments2;
+
+public class AddEmployees {
+	package com.test;
+
+	import java.util.ArrayList;
+	import java.util.List;
+
+	class Employee{
+		
+		private int empid;
+		private String ename;
+		private float sal;
+		
+		public Employee(int empid, String ename, float sal) {
+			super();
+			this.empid = empid;
+			this.ename = ename;
+			this.sal = sal;
+		}
+		
+		public Boolean matchId(int empId) {
+			return this.empid == empId;
+		}
+
+		@Override
+		public String toString() {
+			return "Employee Found[empid=" + empid + ", ename=" + ename + ", sal=" + sal + "]";
+		}
+		
+	}
+
+	public class Program2 {
+		public static void main(String[] args) {
+			List<Employee> employees = new ArrayList<Employee>();
+			
+			System.out.println("Adding Employees");
+			employees.add(new Employee(1, "ABC", 2323));
+			employees.add(new Employee(2, "DEF", 2543));
+			employees.add(new Employee(3, "GHI", 5423));
+			employees.add(new Employee(4, "JKL", 3426));
+			employees.add(new Employee(5, "MNO", 7345));
+			
+			System.out.println("Retrieving Employees");
+			for(Employee emp : employees) {
+				System.out.println(emp);
+			}
+		}
+	}
+}
