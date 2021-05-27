d94aa3ef3e750a27f1fc3f35da747b92d5627d88
AlladaV <AlladaV@HBADT4950711.bsg.ad.adp.com>
2021-05-27 19:26:55 +0530

    Commit

--- SDET Assignments/src/assignments1/CurrentBillCalculationBasedOnNumberOfUnitsConsumption.java
+++ SDET Assignments/src/assignments1/CurrentBillCalculationBasedOnNumberOfUnitsConsumption.java
@@ -1,30 +1,55 @@
 package assignments1;
 
+import java.util.Scanner;
+
 public class CurrentBillCalculationBasedOnNumberOfUnitsConsumption {
-	int n = 0,count=0;
-	String a;
-	Scanner sc = new Scanner(System.in);
-	System.out.println("Enter House or Commercial: ");
-	a=sc.nextLine();
-	System.out.println("Enter no.of units: ");
-	n=sc.nextInt();
-	if(a.toLowerCase().contains("house"))
-	{
-		if(n>1 && n<100)
-		{
-			count=n*3;
-		}
-		else
-			count=n*4;
-	}
-	else
-	{
-		if(n>1 && n<100)
-		{
-			count=n*10;
-		}
-		else
-			count=n*20;
-	}
-	System.out.println("Final billing amount: "+count);
+
+       public String CustomerType;
+       public int UnitsCount;
+       public long BillValue;
+       Scanner scn = new Scanner(System.in);
+       public String getCustomerType() {
+           System.out.println("Enter the CustomerType : ");
+              CustomerType = scn.next();
+              return CustomerType;
+       }
+       
+       public int getUnitsCount() {
+           System.out.println("Enter the Units charged : ");
+           UnitsCount = scn.nextInt();
+              return UnitsCount;
+       }
+       
+       public void calculateBillValue(String CustomerType, int UnitsCount) {
+              if(CustomerType.equals("domestic")) 
+              {
+                     if(UnitsCount <= 100) {
+                           BillValue = UnitsCount * 3;
+                           
+                           System.out.println("Customer Type :" + CustomerType + "\n" + "Units Chaged :" + UnitsCount + "\n" + "Bill Value : " + BillValue  );
+                     }
+                     else if(UnitsCount > 100 & UnitsCount < 200) {
+                           BillValue = UnitsCount * 4;
+                           System.out.println("Customer Type :" + CustomerType + "\n" + "Units Chaged :" + UnitsCount + "\n" + "Bill Value : " + BillValue  );
+                     }
+                     else  {
+                           System.out.println("Invalid units count provided."  );
+                     }
+              }
+              else if(CustomerType.equals( "commercial")) {
+                     if(UnitsCount <= 100) {
+                           BillValue = UnitsCount * 10;
+                           System.out.println("Customer Type :" + CustomerType + "\n" + "Units Chaged :" + UnitsCount + "\n" + "Bill Value : " + BillValue  );
+                     }
+                     else if(UnitsCount > 100 & UnitsCount < 200) {
+                           BillValue = UnitsCount * 20;
+                           System.out.println("Customer Type :" + CustomerType + "\n" + "Units Chaged :" + UnitsCount + "\n" + "Bill Value : " + BillValue  );
+                     }
+                     else  {
+                           System.out.println("Invalid units count provided."  );
+                     }
+              }
+                     
+                     }
+       
 }
